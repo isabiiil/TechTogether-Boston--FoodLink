32ab6785858e2185565bc4fb29ff2408db2c1cd7
Moody Rahman <mrahman01@stuy.edu>
2020-11-06 19:58:01 -0500

    databse schema start

---
+++ api/db.py
@@ -0,0 +1,17 @@
+import mongoengine as mg
+from os import environ
+
+mg.connect("pantry")
+
+class User(mg.Document):
+	username = mg.StringField()
+	password = mg.StringField()
+	organization = mg.StringField()
+	location = mg.StringField()
+	type = mg.StringField()
+	posts = mg.ListField(mg.ReferenceField(Post))
+
+class Post(mg.Document):
+	type = mg.StringField()
+	timestramp = mg.DateField()
+	content = mg.StringField()
