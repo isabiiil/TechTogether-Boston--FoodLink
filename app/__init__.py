9fc3433f86571ff361db508111a35c52ec7c352f
Moody Rahman <mrahman01@stuy.edu>
2020-11-07 07:38:35 -0500

    death to react

--- app/__init__.py
+++ app/__init__.py
@@ -2,24 +2,8 @@ from flask import *
 import json
 import importlib
 import requests
-import mongoengine as mg
 from os import environ
-
-mg.connect("pantry", host=environ["atlasurl"])
-class User(mg.Document):
-	username = mg.StringField()
-	password = mg.StringField()
-	organization = mg.StringField()
-	location = mg.StringField()
-	type = mg.StringField()
-	posts = mg.ListField(mg.ReferenceField("Post"))
-
-
-class Post(mg.Document):
-	type = mg.StringField()
-	timestamp = mg.DateField()
-	content = mg.StringField()
-
+from .util import dbctrl
 
 
 app = Flask(__name__)
@@ -39,8 +23,6 @@ def debug():
 	z = requests.post("http://localhost:5000/login", json={"HELLO":"DIE"})
 	User(username="mood").save()
 	return json.dumps(z.json())
-
-
 
 
 if __name__ == "__main__":
---
+++ app/util/dbctrl.py
@@ -0,0 +1,19 @@
+import mongoengine as mg
+from os import environ
+
+mg.connect("pantry", host=environ["atlasurl"])
+
+
+class User(mg.Document):
+	username = mg.StringField()
+	password = mg.StringField()
+	organization = mg.StringField()
+	location = mg.StringField()
+	type = mg.StringField()
+	posts = mg.ListField(mg.ReferenceField("Post"))
+
+
+class Post(mg.Document):
+	type = mg.StringField()
+	timestamp = mg.DateField()
+	content = mg.StringField()
