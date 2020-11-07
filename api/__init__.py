398b167b0923b08d3d8f6d03dd55091ba82608a4
Moody Rahman <mrahman01@stuy.edu>
2020-11-06 20:14:54 -0500

    api boilerplate

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,23 @@
 from flask import *
-
+import json
+import importlib
+import requests
+importlib.import_module("db")
 
 app = Flask(__name__)
 
+@app.route("/login", methods=["POST"])
+def login():
+	m = request.get_json()
+	out = {}
+	return json.dumps(out)
 
+@app.route("/debug", methods=["GET"])
+def debug():
+	requests.post("http://localhost:5000/login", json={"hello":"bye"})
+	return "h"
 
 
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
