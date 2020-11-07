b9f9acf423ffddb2bb1dfbefb120e906df6681c9
Moody Rahman <mrahman01@stuy.edu>
2020-11-06 20:53:34 -0500

    databse and debugs

--- api/__init__.py
+++ api/__init__.py
@@ -2,9 +2,32 @@ from flask import *
 import json
 import importlib
 import requests
-importlib.import_module("db")
+import mongoengine as mg
+from os import environ
+
+mg.connect("pantry", host=environ["atlasurl"])
+class User(mg.Document):
+	username = mg.StringField()
+	password = mg.StringField()
+	organization = mg.StringField()
+	location = mg.StringField()
+	type = mg.StringField()
+	posts = mg.ListField(mg.ReferenceField("Post"))
+
+
+class Post(mg.Document):
+	type = mg.StringField()
+	timestamp = mg.DateField()
+	content = mg.StringField()
+
+
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+	print(environ["atlasurl"])
+	return "home"
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -14,8 +37,9 @@ def login():
 
 @app.route("/debug", methods=["GET"])
 def debug():
-	requests.post("http://localhost:5000/login", json={"hello":"bye"})
+	User(username="mood").save()
 	return "h"
+
 
 
 
